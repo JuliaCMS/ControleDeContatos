5b350c1db0895479be87c9d5bfe86a30f43def00
JuliaCMS <julia.cms@outlook.com>
2023-03-02 23:57:44 -0300

    conectando ao banco de dados pt1

---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    // herdar o contexto do entity framework do  banco de dados  com : DbContext (precisa adicionar a referencia do dbcontext também)
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
